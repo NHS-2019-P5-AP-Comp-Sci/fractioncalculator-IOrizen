66e52ecd482c9607531e7e112329b977175ab79b
s-chener <s-chener@53S50030338.it.bsd405.org>
2019-11-12 15:30:46 -0800

    2nd checkpoint

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -54,7 +54,66 @@ public class FracCalc {
 		for (int i = 0; i < input.length() - 3 - numsBefore; i++) {
 			secondFraction = secondFraction + input.charAt(3 + numsBefore + i);
 		}
-		return "Second fraction:" + secondFraction;
+		
+		int firstWhole = Integer.parseInt(whole(firstFraction));
+		int secondWhole = Integer.parseInt(whole(secondFraction));
+		int firstNumerator = Integer.parseInt(numerator(firstFraction));
+		int secondNumerator = Integer.parseInt(numerator(secondFraction));
+		int firstDenominator = Integer.parseInt(denominator(firstFraction));
+		int secondDenominator = Integer.parseInt(denominator(secondFraction));
+
+		return "whole:" + secondWhole + " numerator:" +secondNumerator + " denominator:"+secondDenominator;
+	}
+	public static String whole(String fraction) {
+		String whole = "";
+		if (fraction.indexOf("/") == -1) {
+			whole = fraction;
+		} else if (fraction.indexOf("_") != -1) {
+			int numsBeforeUnderscore = fraction.indexOf("_");
+			for (int i = 0; i < numsBeforeUnderscore; i++) {
+				whole = whole + fraction.charAt(i);
+			}
+		} else if (fraction.indexOf("_") == -1) {
+			whole = "0";
+		}
+		return whole;
+	}
+
+	public static String numerator(String fraction) {
+		String numerator = "";
+		if (fraction.indexOf("/") == -1) {
+			numerator = "0";
+		} else if (fraction.indexOf("_") != -1) {
+			int numsBeforeSlash = fraction.indexOf("/");
+			int numsBeforeUnderscore = fraction.indexOf("_");
+			for (int i = 0; i < numsBeforeSlash - numsBeforeUnderscore - 1; i++) {
+				numerator = numerator + fraction.charAt(numsBeforeUnderscore + 1 + i);
+			}
+		} else if (fraction.indexOf("_") == -1) {
+			int numsBeforeSlash = fraction.indexOf("/");
+			for (int i = 0; i < numsBeforeSlash; i++) {
+				numerator = numerator + fraction.charAt(i);
+			}
+		}
+		return numerator;
+	}
+
+	public static String denominator(String fraction) {
+		String denominator = "";
+		if (fraction.indexOf("/") == -1) {
+			denominator = "1";
+		} else if (fraction.indexOf("_") != -1) {
+			int numsBeforeSlash = fraction.indexOf("/");
+			for (int i = 0; i < fraction.length() - numsBeforeSlash - 1; i++) {
+				denominator = denominator + fraction.charAt(numsBeforeSlash + 1 + i);
+			}
+		} else if (fraction.indexOf("_") == -1) {
+			int numsBeforeSlash = fraction.indexOf("/");
+			for (int i = 0; i < fraction.length() - numsBeforeSlash - 1; i++) {
+				denominator = denominator + fraction.charAt(numsBeforeSlash + 1 + i);
+			}
+		}
+		return denominator;
 	}
 
 	// TODO: Fill in the space below with any helper methods that you think you will
